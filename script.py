f9f7f9dd63bb06fc36a4cdbbd616b5ac9fd89bbe
Ariyo Apakama <kingriyor@apples-MacBook-Pro.local>
2020-10-22 14:41:57 +0100

    updated basic script

--- script.py
+++ script.py
@@ -1,25 +1,32 @@
 import requests
 from config import config
 
-base_url = config['tms']['base_url']
-api_key = config['tms']['key']
-lineupId = config['tms']['lineupId']
-zip_code = config['tms']['zip_code']
-startDateTime = "2020-10-22" # current date
 
-# TV 
-# url = base_url + "/movies/airings?zip=" + zip_code + "&api_key=" + api_key + "&lineupId=" + lineupId + "&startDateTime=" + startDateTime
+class script:
+    def __init__(self):
+        self.base_url = config['tms']['base_url']
+        self.api_key = config['tms']['key']
+        self.lineupId = config['tms']['lineupId']
+        self.zip_code = config['tms']['zip_code']
+        self.startDateTime = "2020-10-22" # current date
 
-# Theatre
-url = base_url + "/movies/showings?zip=" + zip_code + "&api_key=" + api_key + "&line_up_id=" + lineupId + "&startDate=" + startDateTime
+    def getTVMovies(self):
+        url = self.base_url + "/movies/airings?zip=" + self.zip_code + "&api_key=" + self.api_key + "&lineupId=" + self.lineupId + "&startDateTime=" + self.startDateTime
+        payload = {}
+        headers= {}
+        response = requests.request("GET", url, headers=headers, data = payload)
+        # print(response.text.encode('utf8'))
+        print(response.json()[1])
+
+    def getTheatreMovies(self):
+        url = self.base_url + "/movies/showings?zip=" + self.zip_code + "&api_key=" + self.api_key + "&line_up_id=" + self.lineupId + "&startDate=" + self.startDateTime
+        payload = {}
+        headers= {}
+        response = requests.request("GET", url, headers=headers, data = payload)
+        # print(response.text.encode('utf8'))
+        print(response.json()[1])
 
 
 
-payload = {}
-headers= {}
+script().getTVMovies()
 
-response = requests.request("GET", url, headers=headers, data = payload)
-
-# print(response.text.encode('utf8'))
-
-print(response.json()[1])
