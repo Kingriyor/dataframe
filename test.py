ddbf2e7a29f06cb1d167a5ef94c69252a959dfb3
Ariyo Apakama <kingriyor@apples-MacBook-Pro.local>
2020-10-25 16:26:30 +0100

    updated tms key

--- test.py
+++ test.py
@@ -25,6 +25,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertTrue(connected)
 
     def test_tms_tvUrl(self):
+        # If this test fails with a response.status_code = 403, this is most likely because the TMS key has been used more than 50 times today. Simply add another TMS key to the config.py file
         url = self.base_url + "/movies/airings?zip=" + self.zip_code + "&api_key=" + self.api_key + "&lineupId=" + self.lineupId + "&startDateTime=" + self.startDateTime
         try:
             response = requests.request("GET", url, timeout=10, headers={}, data = {})
@@ -34,6 +35,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_tms_theatreUrl(self):
+        # If this test fails with a response.status_code = 403, this is most likely because the TMS key has been used more than 50 times today. Simply add another TMS key to the config.py file
         url = self.base_url + "/movies/showings?zip=" + self.zip_code + "&api_key=" + self.api_key + "&startDate=" + self.startDateTime
         try:
             response = requests.request("GET", url, timeout=10, headers={}, data = {})
